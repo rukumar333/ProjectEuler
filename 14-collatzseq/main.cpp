4dca59482384ddd60de2fdb71dcd7fe0bce78c20
Rushil <rukumar333@gmail.com>
2016-03-15 13:23:48 -0400

    finished problem 14

--- 14-collatzseq/main.cpp
+++ 14-collatzseq/main.cpp
@@ -4,6 +4,7 @@
 
 int runSeq(long currentNum, int count, std::vector<long> & listNumbers);
 int length = 5000000;
+int maxes[5000001];
 int numbers[5000001];
 
 int main(){
@@ -11,71 +12,50 @@ int main(){
     std::string temp = "";
     std::getline(std::cin, temp);
     numCases = std::stoi(temp);
-    int starting = 0;
-    int count = 0;
     std::vector<long> vectorNums;
-    // int inputs[numCases];
-    // int maxCount = 0;
-    // int maxBase = 0;    
     for(unsigned int i = 0; i <= length; ++ i){
 	numbers[i] = 0;
+	maxes[i] = 0;
     }
-    for(unsigned int i = length; i >= 1; -- i){
+    int maxBase = 0;
+    int maxCount = 0;
+    int starting = 0;
+    int count = 0;
+    for(unsigned int i = 1; i <= length; ++ i){
 	vectorNums = std::vector<long>();
 	count = runSeq((long)i, 0, vectorNums);
-	numbers[i] = count;
-	for(int j = 0; j < vectorNums.size(); ++ j){
+	if(maxCount <= count){
+	    maxCount = count;
+	    maxBase = i;
+	}	
+	maxes[i] = maxBase;
+	bool recurse = true;
+	for(int j = 0; j < vectorNums.size() && recurse; ++ j){
 	    if(vectorNums[j] <= length){
-		numbers[vectorNums[j]] = count;
+		if(numbers[vectorNums[j]] != 0){
+		    recurse = false;
+		}else{
+		    numbers[vectorNums[j]] = count;
+		}
 	    }
 	    -- count;
 	}
     }
+    int inputs[numCases];
     for(unsigned int i = 0; i < numCases; ++ i){
 	std::getline(std::cin, temp);
 	starting = std::stoi(temp);
-	vectorNums = std::vector<long>();
-	count = runSeq((long)starting, 0, vectorNums);
-	numbers[starting] = count;
-	for(int j = 0; j < vectorNums.size(); ++ j){
-	    if(vectorNums[j] <= length){
-		numbers[vectorNums[j]] = count;
-	    }
-	    -- count;
-	}
-
-	// std::getline(std::cin, temp);
-	// starting = std::stoi(temp);
-	// int max = 0;
-	// int maxNum = 0;
-	// for(int k = starting; k >= 1; -- k){
-	//     if(max < numbers[k]){
-	// 	max = numbers[k];
-	// 	maxNum = k;
-	//     }
-	// }
-	// std::cout << maxNum << std::endl;
+	inputs[i] = starting;
+	std::cout << maxes[starting] << std::endl;
     }
-    // for(unsigned int i = 0; i < numCases; ++ i){
-    // 	starting = inputs[i];
-    // 	int max = 0;
-    // 	int maxNum = 0;
-    // 	for(int k = starting; k >= 1; -- k){
-    // 	    if(max < numbers[k]){
-    // 		max = numbers[k];
-    // 		maxNum = k;
-    // 	    }
-    // 	}
-    // 	std::cout << maxNum << std::endl;
-    // }
     return 1;
 }
 
 int runSeq(long currentNum, int count, std::vector<long> & listNumbers){
     if(currentNum <= length){
-	if(numbers[currentNum] != 0){
-	    return count + numbers[currentNum];
-	}	
+    	if(numbers[currentNum] != 0){
+    	    return count + numbers[currentNum];
+    	}	
     }
     ++ count;
     listNumbers.push_back(currentNum);
@@ -86,8 +66,7 @@ int runSeq(long currentNum, int count, std::vector<long> & listNumbers){
 	currentNum = currentNum / 2;
 	return runSeq(currentNum, count, listNumbers);
     }else{
-	// std::cout << "Is odd" << std::endl;
 	currentNum = (3 * currentNum) + 1;
-	return runSeq(currentNum, count, listNumbers);	
+	return runSeq(currentNum, count, listNumbers);
     }
 }
